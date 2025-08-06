451c0b20f410b086ba657d8d46ccb83937bd2ac1
Klaudio Eduardo <klaudiocarvalho5@gmail.com>
2025-08-06 16:48:42 -0300

    criação de endpoints

--- src/main/java/com/klaudiodev/financesis/financesis/controller/UserController.java
+++ src/main/java/com/klaudiodev/financesis/financesis/controller/UserController.java
@@ -1,41 +1,78 @@
 package com.klaudiodev.financesis.financesis.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+import com.klaudiodev.financesis.financesis.dto.UserDTO;
 import com.klaudiodev.financesis.financesis.model.User;
 import com.klaudiodev.financesis.financesis.service.UserService;
 
-import lombok.Getter;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	@Autowired
-	public UserService userService;
+	private UserService userService;
 
 	@PostMapping
 	public ResponseEntity<?> createUser(@RequestBody User user) {
 		if (userService.existsByEmail(user.getEmail())) {
-			return ResponseEntity.badRequest().body("Email ja está em uso.");
+			return ResponseEntity.badRequest().body("Email já está em uso");
 		}
-		
 		return ResponseEntity.ok(userService.save(user));
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
+		Optional<User> user = userService.findById(id);
+		return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 	@GetMapping
-	public List<User> getAllUsers() {
-	    return userService.findAll();
+	public ResponseEntity<List<UserDTO>> getAllUsers() {
+	    return ResponseEntity.ok(userService.findAllUsers());
+	}
+
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+		return userService.findById(id).map(user -> {
+			user.setName(updatedUser.getName());
+			user.setEmail(updatedUser.getEmail());
+			user.setCpf(updatedUser.getCpf());
+			userService.save(user);
+			return ResponseEntity.ok(user);
+		}).orElse(ResponseEntity.notFound().build());
 	}
 	
-	
+	@GetMapping("/search")
+	public ResponseEntity<?> getUserByEmail(@RequestParam String email) {
+	    return userService.findByEmail(email)
+	           .map(ResponseEntity::ok)
+	           .orElse(ResponseEntity.notFound().build());
+	}
+
 
 }
--- src/main/java/com/klaudiodev/financesis/financesis/service/UserService.java
+++ src/main/java/com/klaudiodev/financesis/financesis/service/UserService.java
@@ -2,10 +2,13 @@ package com.klaudiodev.financesis.financesis.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.klaudiodev.financesis.financesis.dto.UserDTO;
 import com.klaudiodev.financesis.financesis.model.User;
 import com.klaudiodev.financesis.financesis.repository.UserRepository;
 
@@ -14,13 +17,12 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	public User save(User user) {
 		return userRepository.save(user);
-	}
-
-	public List<User> findAll(User user) {
-		return userRepository.findAll();
 	}
 
 	public Optional<User> findById(Long id) {
@@ -38,5 +40,14 @@ public class UserService {
 	public boolean existsByEmail(String email) {
 		return userRepository.existsByEmail(email);
 	}
+	
+	public List<UserDTO> findAllUsers() {
+	    return userRepository.findAll()
+	            .stream()
+	            .map(user -> modelMapper.map(user, UserDTO.class))
+	            .collect(Collectors.toList());
+	}
+
+
 
 }
